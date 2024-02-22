b63423b3fad590e2e3f983fd72b7e3e4342a37ed
AlexX1-dev <Alex_X1@online.de>
2024-02-22 16:46:41 +0100

    Cleanup, shadow jar, and gitignore

--- src/main/java/de/tmexner/mqtt/chat/java/MQTTListener.java
+++ src/main/java/de/tmexner/mqtt/chat/java/MQTTListener.java
@@ -4,50 +4,46 @@ import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class MQTTListener implements MqttCallback {
-    private final String ip;
-    private final int port;
-    private final String username;
+  private final String username;
 
-    private final ChatClientGUI gui;
+  private final ChatClientGUI gui;
 
-    private final String broker;
+  private final String broker;
 
-    private final String topic;
-    public MQTTListener(String ip, int port, String username, ChatClientGUI gui) {
-        this.ip = ip;
-        this.port = port;
-        this.username = username;
-        this.broker = "tcp://" + ip + ":" + port;
-        this.topic = "mqtt-chat/" + username + "/#";
-        this.gui = gui;
-    }
+  private final String topic;
 
-    void subscribe() throws MqttException {
-        MemoryPersistence persistence = new MemoryPersistence();
-        MqttClient client = new MqttClient(broker, username, persistence);
-        MqttConnectOptions connOpts = new MqttConnectOptions();
-        connOpts.setCleanSession(true);
-        client.connect(connOpts);
-        client.setCallback(this);
-        client.subscribe(topic);
+  public MQTTListener(String ip, int port, String username, ChatClientGUI gui) {
+    this.username = username;
+    this.broker = "tcp://" + ip + ":" + port;
+    this.topic = "mqtt-chat/" + username + "/#";
+    this.gui = gui;
+  }
 
-    }
+  void subscribe() throws MqttException {
+    MemoryPersistence persistence = new MemoryPersistence();
+    MqttClient client = new MqttClient(broker, username, persistence);
+    MqttConnectOptions connOpts = new MqttConnectOptions();
+    connOpts.setCleanSession(true);
+    client.connect(connOpts);
+    client.setCallback(this);
+    client.subscribe(topic);
 
+  }
 
-    @Override
-    public void connectionLost(Throwable throwable) {
+  @Override
+  public void connectionLost(Throwable throwable) {
 
-    }
+  }
 
-    @Override
-    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-        String message = new String(mqttMessage.getPayload());
-        gui.receiveMQTTMessage(s, message);
+  @Override
+  public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+    String message = new String(mqttMessage.getPayload());
+    gui.receiveMQTTMessage(s, message);
 
-    }
+  }
 
-    @Override
-    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+  @Override
+  public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
 
-    }
+  }
 }
--- src/main/java/de/tmexner/mqtt/chat/java/MQTTService.java
+++
@@ -1,7 +0,0 @@
-package de.tmexner.mqtt.chat.java;
-
-import org.eclipse.paho.client.mqttv3.MqttClient;
-
-public class MQTTService {
-  private MqttClient client;
-}
